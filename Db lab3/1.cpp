101506110e924fefc475aea1722a89ac44870340
Yerasyl04 <91834677+Yerasyl04@users.noreply.github.com>
2022-10-06 15:31:31 +0600

    Update 1.cpp

--- Db lab3/1.cpp
+++ Db lab3/1.cpp
@@ -1,5 +1,10 @@
 Difference between DDL and DMLDDL:DDL is Data Definition Language which is used to define data structures.
 For example: create table, alter table are instructions in SQL.DML:DDL is Data Manipulation Language which is used to manipulate data itself.
 For example: insert, update, delete are instructions in SQL.List of DDL commands: •CREATE: This command is used to create the database or its objects (like table, index, function, views, store procedure, and triggers).•DROP:
-This command is used to delete objects from the database.•ALTER: This is used to alter the structure of the database.•TRUNCATE: This  is  used  to  remove  all  records  from  a  table,  including  all  spaces allocated for the records are removed.•COMMENT: This is used to add comments to the data dictionary.•RENAME: This is used to rename an object existing in the database.List of DML commands: •INSERT: It is used to insert data into a table.•UPDATE:It is used to update existing data within a table.•DELETE: It is used to delete records from a database table.•LOCK:Table control concurrency.•CALL: Call a PL/SQL or JAVA subprogram.
+This command is used to delete objects from the database.•ALTER: This is used to alter the structure of the database.
+  •TRUNCATE: This  is  used  to  remove  all  records  from  a  table,  including  all  spaces allocated for the records are removed.
+ •COMMENT: This is used to add comments to the data dictionary.•RENAME: This is used to rename an object existing in the database.List of DML commands: 
+•INSERT: It is used to insert data into a table.•UPDATE:It is used to update existing data within a table.
+  •DELETE: It is used to delete records from a database table.•LOCK:Table control concurrency.
+    •CALL: Call a PL/SQL or JAVA subprogram.
 •EXPLAIN PLAN:It describes the access path to data.
